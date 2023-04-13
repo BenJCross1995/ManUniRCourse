4d052fa0bc49ba2262d81362fa8a0e180bbea609
Ben.Cross <benjcross95@gmail.com>
2023-04-13 10:26:45 +0100

    Create R Course Notes.R

---
+++ src/R Course Notes.R
@@ -0,0 +1,9 @@
+#----------------------------------------#
+#-----MANCHESTER UNIVERSITY R COURSE-----#
+#---------13/04/2023-14/04/2023----------#
+#----------------------------------------#
+
+# Course Notes - https://uomresearchit.github.io/r-tidyverse-intro/setup/
+# Google Docs - https://docs.google.com/document/d/1nFisfUKHSrZeAkEKCVKBEhnhduB_cNzoZ65VBsGYqPQ/edit
+
+#----------DAY 1----------#
